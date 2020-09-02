bfa4c59f3fb17f30ecbd036205627661702bb185
APPLE <apple@abedkiloo.local>
2020-09-02 13:26:15 +0300

    üpdate of dynamic array implementation

--- dynamic_array.py
+++ dynamic_array.py
@@ -1,5 +1,4 @@
-# create an array
-my_array=[]
+# create an implementation of dynamic array 
 class MyArray:
 
 	def __init__(self):
@@ -24,5 +23,14 @@ class MyArray:
 		for i in self.array:
 			new_array[i]=self.array[i] # copy element of previous array to new
 		return new_array
+
+	def get(self,index):
+		if index <0 or index>self.current_index:
+			raise Exception("Index not found")
+		return self.array[index]
+
+	def size(self):
+		return len(self.array)
+
 
 		
