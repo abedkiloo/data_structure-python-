56be18e63211c376984e1774d9353dbc498559a4
APPLE <apple@abedkiloo.local>
2020-09-03 23:20:15 +0300

    adding items to a start or end of node or after another node

--- single_linked_list.py
+++ single_linked_list.py
@@ -21,3 +21,35 @@ class LinkedList:
 		new_node=Node(data)
 		new_node.ref=self.start_node
 		self.start_node=new_node
+
+	def insert_at_end(self,data):
+		new_node= Node(data)
+		if self.start_node is None:
+			self.start_node=new_node
+			return
+		n=self.start_node
+		while n.ref is not None:
+			n=n.ref
+		n.ref=new_node
+
+	def insert_after_item(self ,x,data):
+		n=self.start_node
+		print(n.ref)	
+		while n is not None:
+			if n.item==x:
+				break
+			n=n.ref
+		if n is None:
+			print("item not found")
+		else:
+			new_node=Node(data)
+			new_node.ref=n.ref
+			n.ref=new_node
+
+
+
+
+
+
+
+
